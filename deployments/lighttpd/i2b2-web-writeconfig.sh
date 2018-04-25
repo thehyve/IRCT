2de2012aa80e01f5f9ccb334f3339ed6dba4e5ad
Mickaël Misbach <mickael@thehyve.nl>
2018-04-25 13:50:23 +0200

    add deployment for test systems

---
+++ deployments/lighttpd/i2b2-web-writeconfig.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -e
+
+cat > "$LIGHTTPD_WEB_ROOT/i2b2-admin/i2b2_config_data.js" <<EOL
+{
+    urlProxy: "index.php",
+    urlFramework: "js-i2b2/",
+
+    lstDomains: [ {
+        domain: "$I2B2_DOMAIN_NAME",
+        name: "Domain $I2B2_DOMAIN_NAME",
+        urlCellPM: "http://wildfly-server:8080/i2b2/services/PMService/",
+        allowAnalysis: true,
+        adminOnly: true,
+        debug: false
+    } ]
+}
+EOL
+
+cat > "$LIGHTTPD_WEB_ROOT/i2b2-client/i2b2_config_data.js" <<EOL
+{
+    urlProxy: "index.php",
+    urlFramework: "js-i2b2/",
+
+    lstDomains: [  {
+        domain: "$I2B2_DOMAIN_NAME",
+        name: "Domain $I2B2_DOMAIN_NAME",
+        urlCellPM: "http://wildfly-server:8080/i2b2/services/PMService/",
+        allowAnalysis: true,
+        debug: false
+    } ]
+}
+EOL
+
+cat > "$LIGHTTPD_WEB_ROOT/index.html" <<EOL
+<html><head>
+<title>Index</title>
+</head><body>
+
+<div align="center">
+<p><a href="/i2b2-admin">I2b2 admin</a></p>
+<p><a href="/i2b2-client">I2b2 client</a></p>
+<p><a href="/phppgadmin">PhpPgAdmin</a></p>
+<p><a href=":9990">WildFly Management</a></p>
+<p><a href=":8080/i2b2">I2b2 Axis2 Management</a></p>
+</div>
+
+</body>
+</html>
+EOL
+
+# webclients whitelist URLs
+sed -i "s/\"http:\/\/localhost\"/\"http:\/\/wildfly-server:8080\"/" "$LIGHTTPD_WEB_ROOT/i2b2-admin/index.php"
+sed -i "s/\"http:\/\/localhost\"/\"http:\/\/wildfly-server:8080\"/" "$LIGHTTPD_WEB_ROOT/i2b2-client/index.php"
---
+++ deployments/postgresql/initdb-data/i2b2/10-i2b2-pm-configuration.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+# configuration of the i2b2 project management cell
+
+####################################################################################
+######################### information about password hash ##########################
+####################################################################################
+
+### how to generate the hash (from shrine sources folder)
+# cd "install/i2b2-1.7/i2b2"
+# javac ./I2b2PasswordCryptor.java
+# SHRINE_PW=$(java -classpath ./ I2b2PasswordCryptor <thepassword>)
+
+### some encrypted versions:
+# demouser=             9117d59a69dc49807671a51f10ab7f
+# prigen2017=           f8eb764674b57b5710e3c1665464e29
+# pFjy3EjDVwLfT2rB9xkK= 7cb1ac9deab165535494d60da1d3d7e
+####################################################################################
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$I2B2_DEMO_DB_NAME" <<-EOSQL
+
+    -- cell parameters
+    insert into i2b2pm.pm_cell_params (datatype_cd, cell_id, project_path, param_name_cd, value, changeby_char, status_cd) values
+        ('T', 'FRC', '/', 'DestDir', '$I2B2_FR_FILES_DIR', 'i2b2', 'A');
+
+    -- database lookups
+    update i2b2hive.crc_db_lookup SET C_DOMAIN_ID = '$I2B2_DOMAIN_NAME' WHERE C_DOMAIN_ID = 'i2b2demo';
+    UPDATE i2b2hive.im_db_lookup SET C_DOMAIN_ID = '$I2B2_DOMAIN_NAME' WHERE C_DOMAIN_ID = 'i2b2demo';
+    UPDATE i2b2hive.ont_db_lookup SET C_DOMAIN_ID = '$I2B2_DOMAIN_NAME' WHERE C_DOMAIN_ID = 'i2b2demo';
+    UPDATE i2b2hive.work_db_lookup SET C_DOMAIN_ID = '$I2B2_DOMAIN_NAME' WHERE C_DOMAIN_ID = 'i2b2demo';
+
+    -- hive & users data
+    UPDATE i2b2pm.pm_hive_data SET DOMAIN_ID = '$I2B2_DOMAIN_NAME', DOMAIN_NAME = '$I2B2_DOMAIN_NAME' WHERE DOMAIN_ID = 'i2b2';
+
+    UPDATE i2b2pm.PM_CELL_DATA SET URL = 'http://wildfly-server:8080/i2b2/services/QueryToolService/' WHERE CELL_ID = 'CRC';
+    UPDATE i2b2pm.PM_CELL_DATA SET URL = 'http://wildfly-server:8080/i2b2/services/FRService/' WHERE CELL_ID = 'FRC';
+    UPDATE i2b2pm.PM_CELL_DATA SET URL = 'http://wildfly-server:8080/i2b2/services/OntologyService/' WHERE CELL_ID = 'ONT';
+    UPDATE i2b2pm.PM_CELL_DATA SET URL = 'http://wildfly-server:8080/i2b2/services/WorkplaceService/' WHERE CELL_ID = 'WORK';
+    UPDATE i2b2pm.PM_CELL_DATA SET URL = 'http://wildfly-server:8080/i2b2/services/IMService/' WHERE CELL_ID = 'IM';
+
+    UPDATE i2b2pm.PM_USER_DATA SET PASSWORD = '9117d59a69dc49807671a51f10ab7f' WHERE USER_ID = 'i2b2';
+    UPDATE i2b2pm.PM_USER_DATA SET PASSWORD = '9117d59a69dc49807671a51f10ab7f' WHERE USER_ID = 'demo';
+    UPDATE i2b2pm.PM_USER_DATA SET PASSWORD = '7cb1ac9deab165535494d60da1d3d7e' WHERE USER_ID = 'AGG_SERVICE_ACCOUNT';
+EOSQL
